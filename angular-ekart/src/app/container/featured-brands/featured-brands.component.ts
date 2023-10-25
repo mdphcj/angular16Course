410731099acdaf31be545eabe2ec20b5374e1269
mdphcj <mehul.java@gmail.com>
2023-10-25 12:27:27 +0530

    progress

--- angular-ekart/src/app/container/featured-brands/featured-brands.component.ts
+++ angular-ekart/src/app/container/featured-brands/featured-brands.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-featured-brands',
+  selector: 'featured-brands',
   templateUrl: './featured-brands.component.html',
   styleUrls: ['./featured-brands.component.css']
 })
