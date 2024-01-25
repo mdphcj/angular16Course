c452a8e7af97530a53572cbb44bd3496d5e30c95
mdphcj <mehul.java@gmail.com>
2024-01-25 22:52:13 +0530

    @Input in custom directive

--- angular-ekart/src/app/CustomDirectives/SetBackground.directive.ts
+++ angular-ekart/src/app/CustomDirectives/SetBackground.directive.ts
@@ -1,16 +1,23 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
+import { Directive, ElementRef, Input, OnInit, Renderer2 } from "@angular/core";
 
 @Directive({
   selector: '[setBackground]'
 })
 export class SetBackground implements OnInit {
+
+  // @Input('setBackground') backColor: string = "#36454F";
+  // @Input() textColor: string = "white";
+  @Input('setBackground') changeTextAndBackColor: {
+    backColor: string, textColor: string
+  };
+
   constructor(private element: ElementRef, private renderer: Renderer2) {
   }
   ngOnInit() {
     // this.element.nativeElement.style.backgroundColor = '#36454F';
     // this.element.nativeElement.style.color = 'white';
-    this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', '#36454F');
-    this.renderer.setStyle(this.element.nativeElement, 'color', 'white');
+    this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', this.changeTextAndBackColor.backColor);
+    this.renderer.setStyle(this.element.nativeElement, 'color', this.changeTextAndBackColor.textColor);
     // this.renderer.setAttribute(this.element.nativeElement, 'title', 'This is example title.');
   }
 }
